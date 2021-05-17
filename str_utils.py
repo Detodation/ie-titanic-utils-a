3556ed254460dcd306fc889ed0d99f000a3bb372
Ignacio Martínez de Toda <imartinezdetoda@student.ie.edu>
2021-05-17 11:01:53 +0200

    Add tokenize function

---
+++ str_utils.py
@@ -0,0 +1,10 @@
+def tokenize(text):
+    return text.split()
+
+#touch str_utils.py to create the python script
+
+#Defining a function to tokenize the words in the names of the passengers
+
+#python to start python in the terminal
+#exit() to exit from the py script in terminal
+
